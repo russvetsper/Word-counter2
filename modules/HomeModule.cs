100dd5e7c5b959beb2193a8e2e367ecfce1b9b89
russvetsper <russvetsper@gmail.com>
2016-07-30 17:57:20 -0700

    ddd

--- modules/HomeModule.cs
+++ modules/HomeModule.cs
@@ -1,14 +1,20 @@
 using Nancy;
-// using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace WordCounter
 {
   public class HomeModule : NancyModule
   {
     public HomeModule()
-
+     {
       Get["/"] = _ => {
         return View["index.cshtml"];
       };
+
+      Post["/result"] = _ => {
+    
+        return View["results.cshtml"];
+      };
  }
+}
 }
